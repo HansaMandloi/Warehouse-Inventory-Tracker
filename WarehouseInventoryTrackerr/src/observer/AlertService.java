40c38eaa378da7df7f57283af744003e4e0eed44
HansaMandloi <hansamandloi951254@gmail.com>
2025-10-30 11:08:36 +0530

    Fix typo in low stock alert message

--- WarehouseInventoryTrackerr/src/observer/AlertService.java
+++ WarehouseInventoryTrackerr/src/observer/AlertService.java
@@ -5,7 +5,7 @@ import model.Product;
 public class AlertService implements StockObserver {
     @Override
     public void onLowStock(Product product) {
-        System.out.println("⚠️ Restock Alert: Low stock for " + product.getName() +
+        System.out.println("Restock Alert: Low stock for " + product.getName() +
                 " – only " + product.getQuantity() + " left!");
     }
 }
